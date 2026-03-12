d98958b73cab4a5cd9fadb20c735af6da6b5255a
phhnielsen <phhnielsen@gmail.com>
2026-03-12 10:33:28 +0100

    refactor(exercise 1): refactored products into 1 class

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,17 @@
+import {ProductType} from "./ProductType";
+
+export class Product {
+    public title: string;
+    public imageUrl: string;
+    public basePrice: number;
+    public taxRate: number = 1.25;
+    public discountRate: number = 0.10;
+    public productType: ProductType;
+
+    constructor(title: string, imageUrl: string, basePrice: number, productType: ProductType) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.basePrice = basePrice;
+        this.productType = productType;
+    }
+}
