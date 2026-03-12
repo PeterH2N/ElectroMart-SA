ba22fcff9372e3268182dde882fc628341310056
phhnielsen <phhnielsen@gmail.com>
2026-03-12 10:51:51 +0100

    refactor(frontend): refactored product pages to 1 page

---
+++ frontend/src/pages/ProductPage.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {Product} from "../models/Product";
+import {ProductType} from "../models/ProductType";
+
+const Products = ({productType}: { productType: ProductType }) => {
+    const [listOfProducts, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get<Product[]>(`http://localhost:3001/get-products-by-category?productType=${productType.toString()}`);
+                const products = response.data.map(item => new Product(item.title, item.imageUrl, item.basePrice, item.productType));
+                setProducts(products);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, [productType]);
+
+    return (
+        <div className="content-container">
+            <h2>Products</h2>
+            <div className='product-item-container'>
+        {listOfProducts.map((product, index) => (
+                <div className='product-item' key={index}>
+            <img src={product.imageUrl} alt={product.title} style={{ width: '100px', height: '100px' }} />
+    <h3>{product.title}</h3>
+    <p>Price: ${product.getPrice()}</p>
+    </div>
+))}
+    </div>
+    </div>
+);
+
+};
+
+export default Products;
